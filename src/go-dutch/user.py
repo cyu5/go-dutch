76e8d1b78467e92f3a1561e44db841555a614b33
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2021-08-21 16:49:22 -0500

    User class and package naming

---
+++ src/go-dutch/user.py
@@ -0,0 +1,13 @@
+# user entity involved in shared costs
+
+class User:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+        # self.groups = list[Group]
+
+
+    # def groupBalances (self):
+
+    # def getNetBalance (self):
