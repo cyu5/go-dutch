2710abb3c856cde890a07e06e021d937bf470c07
Cheng <cyu5>
2021-08-27 17:01:01 -0500

    privatize group variables and replace group.member with group.balances

--- src/go_dutch/group.py
+++ src/go_dutch/group.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 
 # Group class
-class Group:
+class Group():
 
     # constructor
     def __init__(self, members=None) -> None:
-        self.members = set()
+        self.__balances = dict()
         if members is not None:
             # add members to group
             if not isinstance(members, str):
@@ -15,24 +15,23 @@ class Group:
                     if not isinstance(member, str):
                         raise TypeError(f'{member} is not a str')
                     else:
-                        self.members.add(member)
+                        self.__balances[member] = 0
             else:
                 raise TypeError('input should be an iterable, not a string')
             
-        self.ledger = list()
-        self.balances = defaultdict(int)
+        self.__ledger = list()
 
     def extend_members(self, new_members) -> None:
         if not isinstance(new_members, str):
-            tmp = set()
+            tmp = dict()
             for member in new_members:
 
                 # input validation
                 if not isinstance(member, str):
                     raise TypeError(f'{member} is not a str')
-                else:
-                    tmp.add(member)
-            self.members.update(tmp)
+                elif member not in self.__balances:
+                    tmp[member] = 0
+            self.__balances.update(tmp)
         else:
             raise TypeError('input should be an iterable, not a string')
             
@@ -41,7 +40,24 @@ class Group:
     def add_member (self, member: str) -> None:
 
         if isinstance(member, str):
-            self.members.add(member)
+            self.__balances.setdefault(member, 0)
         else:
             raise TypeError('Single string argument required for input')
     
+    def get_members (self) -> list:
+        return list(self.__balances.keys())
+
+    def get_balance (self, member: str) -> int:
+        try:
+            return self.__balances[member]
+        except KeyError:
+            raise KeyError(f"{member} is not part of group {self}")
+
+    def has_member (self, member: str) -> int:
+        return member in self.__balances
+
+    def get_ledger(self) -> list:
+        return self.__ledger
+
+    def get_balances (self) -> dict:
+        return self.__balances
--- tests/test.py
+++ tests/test.py
@@ -18,13 +18,13 @@ def test_group_init_validation():
 
 def test_group_init_members():
     # check empty members
-    assert Group([]).members.__len__() == 0
+    assert Group([]).get_members().__len__() == 0
 
     # all members unique
-    assert Group(["joe", "nick", "john"]).members.__len__() == 3
+    assert Group(["joe", "nick", "john"]).get_members().__len__() == 3
 
     # check unique members
-    assert Group(["john", "john"]).members.__len__() == 1
+    assert Group(["john", "john"]).get_members().__len__() == 1
 
 
 def test_add_member ():
@@ -35,12 +35,12 @@ def test_add_member ():
 
     # function adds new
     group1.add_member (member1)
-    assert group1.members.__len__() == 1
-    assert group1.balances[member1] == 0
+    assert group1.get_members().__len__() == 1
+    assert group1.get_balance(member1) == 0
 
     # function has no effect if member already exists
     group1.add_member (member1)
-    assert group1.members.__len__() == 1
+    assert group1.get_members().__len__() == 1
 
     # function raises error on invalid input
     with pytest.raises(TypeError):
@@ -64,16 +64,21 @@ def test_extend_members():
         group.extend_members("a string")
     with pytest.raises(TypeError):
         group.extend_members(['joe','john',1])
-    assert len(group.members) == 0
+    assert group.get_members().__len__() == 0
 
     # check empty members
     group.extend_members([])
-    assert group.members.__len__() == 0
+    assert group.get_members().__len__() == 0
 
     # all members unique
     group.extend_members(["joe", "nick", "john"])
-    assert group.members.__len__() == 3
+    assert group.get_members().__len__() == 3
 
     # check unique members
     group.extend_members(["sarah", "sarah"])
-    assert group.members.__len__() == 4
+    assert group.get_members().__len__() == 4
+
+def test_has_member():
+    group = Group(['joe'])
+    assert group.has_member('joe')
+    assert not group.has_member("john")
